493774fec0887ebb8e96e895b5f4e57078727df9
Jare Fagbemi <ofagbemi@stanford.edu>
2016-04-18 21:26:29 -0700

    Update API to search for and return rows
    Allows client to search columns via ‘=‘ or ‘LIKE’ operators

--- routes/api/models/index.js
+++ routes/api/models/index.js
@@ -1,18 +1,53 @@
 'use strict';
 
-const _          = require('underscore');
-
+const _      = require('underscore');
 const Models = require('../../../server/models');
 const router = require('express').Router();
 
 const DATA_TYPES = require('../../../server/models/data-types.json');
+const DEFAULT_ROW_LIMIT = 20;
+const MAX_ROW_LIMIT = 100;
+const FILTER_REGEXES = [{
+  op: 'equal',
+  regex: /(.+)\=\=(.+)/
+}, {
+  op: '$like',
+  regex: /(.+)\=\@(.+)/,
+  transform: (str) => `%${str}%`
+}];
 
 router.get('/', (req, res) => {
   res.json(Models.schemas);
 });
 
-router.get('/:name', (req, res) => {
-  res.json(Models.schemas[req.params.name]);
+router.get('/:model', (req, res, next) => {
+
+  let page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit, 10) || DEFAULT_ROW_LIMIT;
+  if (limit > MAX_ROW_LIMIT) {
+    limit = MAX_ROW_LIMIT;
+  }
+
+  let model = Models.models[req.params.model];
+  let schema = Models.schemas[req.params.model];
+  if (!model) {
+    return next(new Error(`Model ${req.params.model} could not be found`));
+  } else if (!schema) {
+    return next(new Error(`Schema ${req.params.model} could not be found`));
+  }
+
+  let where = processFilters(req.query.filter, schema);
+  model.findAll({
+    limit: limit,
+    offset: (page - 1) * limit,
+    order: [['id', 'ASC']],
+    where: where
+  }).then((rows) => {
+    rows = _.map(rows, (row) => row.dataValues );
+    return res.json(rows);
+  }).catch((err) => {
+    return next(err);
+  });
 });
 
 router.post('/', (req, res, next) => {
@@ -28,3 +63,60 @@ router.post('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+function processFilters(filters, schema) {
+  if (!filters) {
+    return {};
+  } else if (_.isString(filters)) {
+    filters = [filters];
+  } else if (!_.isArray(filters)) {
+    return {};
+  }
+
+  let where = {};
+  _.each(filters, (param) => {
+    let match = null;
+    let op = null;
+    let column = null;
+    let value = null;
+
+    // go through the regex's in order of priority
+    for (let exps of FILTER_REGEXES) {
+      match = param.match(exps.regex);
+      if (match) {
+        op = exps.op;
+        column = match[1];
+
+        // call the transform function or some function
+        // that just returns the match
+        value = (exps.transform || ((val) => val))(match[2]);
+        break;
+      }
+    }
+
+    // if none of the regex's matched, then skip
+    // this filter
+    if (!match) { return; }
+
+    // if the column value isn't a valid member of the schema, skip
+    // over it
+    if (schema && schema._cms_.table.columns.indexOf(column) === -1) {
+      return;
+    }
+
+    let filter = {};
+
+    if (op === 'equal') {
+      filter[column] = value;
+    } else {
+      let ref = filter[column] = {};
+      ref[op] = value;
+    }
+
+    // add this set of filters to the returend
+    // 'where' hash
+    _.extend(where, filter);
+  });
+
+  return where;
+}
