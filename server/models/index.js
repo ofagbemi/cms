4daea13f96de9c320680ed07f834cf9204b118aa
Jare Fagbemi <ofagbemi@stanford.edu>
2016-04-18 21:37:55 -0700

    Cleanup

--- server/models/index.js
+++ server/models/index.js
@@ -34,8 +34,8 @@ class Models {
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASS, {
-        host: '127.0.0.1',
-        dialect: 'mysql'
+          host: '127.0.0.1',
+          dialect: 'mysql'
       });
 
       this._sequelize = sequelize;
@@ -141,7 +141,7 @@ Models.prototype._loadModels = function() {
 
     // just get the JSON files
     files = _.filter(files, (file) => {
-      return !!file.match(jsonRegex);
+      return file.search(jsonRegex) > -1;
     });
 
     // create an array of table names...
