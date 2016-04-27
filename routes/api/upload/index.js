5def63cf5529177b4c3895c1e304c8618af3805d
Jare Fagbemi <ofagbemi@stanford.edu>
2016-04-26 22:12:53 -0700

    Add path sanitization

--- routes/api/upload/index.js
+++ routes/api/upload/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const sanitizeFilename = require('sanitize-filename');
 const multiparty = require('multiparty');
-const sanitize = require('sanitize-filename');
-const pathJoin = require('path').join;
+const pathJoin   = require('path').join;
+const _      = require('underscore');
 const async  = require('async');
+const trim   = require('underscore.string/trim');
 const router = require('express').Router();
 const upload = require('../../../server/upload').upload;
 const resave = require('../../../server/upload').resave;
@@ -28,11 +30,26 @@ function getPathStreamCallback(form) {
         }).on('end', () => {
           let buffer = Buffer.concat(buff);
           let path   = String(buffer);
+          let sanitizedPath = sanitizePath(path);
           return callback(path);
         });
       }
     });
   };
+}
+
+function sanitizePath(path) {
+
+  let pathArr = path.split(/[\/\\]/);
+  pathArr = _.compact(_.map(pathArr, (part) => {
+    let sanitized = sanitizeFilename(part);
+
+    // get rid of empty strings
+    if (!trim(sanitized)) { return null; }
+    return sanitized;
+  }));
+
+  return pathArr.join('/');
 }
 
 router.post('/', (req, res, next) => {
